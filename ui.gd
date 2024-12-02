b99247b696811477ea036311eb769896d84dd701
Vizl87 <ojantakanenaleksi@gmail.com>
2024-12-02 13:11:13 +0200

    ui

---
+++ ui.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+
+
+	
+
+
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
+	
